c775f4546155fcad9a241cdddf06e06dc7bb0507
Reyold Osei Adade <reynoldadade@gmail.com>
2019-06-04 13:34:34 +0000

    set up keys for google auth

--- config/passport.setup.js
+++ config/passport.setup.js
@@ -1,9 +1,12 @@
 const passport = require('passport');
 const googleStrategy = require('passport-google-oauth20');
+const keys  = require('./keys');
 
 passport.use(
     new googleStrategy({
     //options for the strategy
-}), () => {
+    clientID: keys.google.clientID,
+    clientSecret: keys.google.clientSecret
+    }), () => {
     //passport callback pasport
 })
